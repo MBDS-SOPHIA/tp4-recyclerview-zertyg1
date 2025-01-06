b2c7ddf421e1f336f16d0d823068bce106cf1d4e
zertyg1 <abdoxvito@gmail.com>
2025-01-06 16:03:15 +0100

    Gestion des images de profile

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
@@ -7,34 +7,34 @@ object FakeApiServiceGenerator {
 
     @JvmField
     var FAKE_USERS = mutableListOf(
-        User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-        User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-        User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-        User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-        User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-        User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-        User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-        User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-        User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-        User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-        User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-        User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-        User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-        User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-        User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-        User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-        User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-        User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-        User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-        User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+        User("001", "Jake", "https://i.pravatar.cc/150?u=1"),
+        User("002", "Paul", "https://i.pravatar.cc/150?u=2"),
+        User("003", "Phil", "https://i.pravatar.cc/150?u=3"),
+        User("004", "Guillaume", "https://i.pravatar.cc/150?u=4"),
+        User("005", "Francis", "https://i.pravatar.cc/150?u=5"),
+        User("006", "George", "https://i.pravatar.cc/150?u=6"),
+        User("007", "Louis", "https://i.pravatar.cc/150?u=7"),
+        User("008", "Mateo", "https://i.pravatar.cc/150?u=8"),
+        User("009", "April", "https://i.pravatar.cc/150?u=9"),
+        User("010", "Louise", "https://i.pravatar.cc/150?u=10"),
+        User("011", "Elodie", "https://i.pravatar.cc/150?u=11"),
+        User("012", "Helene", "https://i.pravatar.cc/150?u=12"),
+        User("013", "Fanny", "https://i.pravatar.cc/150?u=13"),
+        User("014", "Laura", "https://i.pravatar.cc/150?u=14"),
+        User("015", "Gertrude", "https://i.pravatar.cc/150?u=15"),
+        User("016", "Chloé", "https://i.pravatar.cc/150?u=16"),
+        User("017", "April", "https://i.pravatar.cc/150?u=17"),
+        User("018", "Marie", "https://i.pravatar.cc/150?u=18"),
+        User("019", "Henri", "https://i.pravatar.cc/150?u=19"),
+        User("020", "Rémi", "https://i.pravatar.cc/150?u=20")
     )
 
     @JvmField
     var FAKE_USERS_RANDOM = Arrays.asList(
-        User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-        User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-        User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-        User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-        User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+        User("021", "Lea", "https://i.pravatar.cc/150?u=21"),
+        User("022", "Geoffrey", "https://i.pravatar.cc/150?u=22"),
+        User("023", "Simon", "https://i.pravatar.cc/150?u=23"),
+        User("024", "André", "https://i.pravatar.cc/150?u=24"),
+        User("025", "Leopold", "https://i.pravatar.cc/150?u=25")
     )
 }
